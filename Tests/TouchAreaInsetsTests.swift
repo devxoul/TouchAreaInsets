41a8c3ef2c19cf6e277a80398e39f5c347379da2
seongho-hong <seongho.hong@navercorp.com>
2019-05-30 20:01:31 +0900

    Support accessibilityFrame

--- Tests/TouchAreaInsetsTests.swift
+++ Tests/TouchAreaInsetsTests.swift
@@ -38,5 +38,20 @@ class Tests: XCTestCase {
     XCTAssertEqual(view.accessibilityFrame.contains(CGPoint(x: 105, y: 50)), true)
     XCTAssertEqual(view.accessibilityFrame.contains(CGPoint(x: 50, y: 105)), true)
   }
+  
+  func testTouchArea_withAccessibilityFrame() {
+    let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+    view.accessibilityFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
+    view.touchAreaInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    XCTAssertEqual(view.point(inside: CGPoint(x: 50, y: -5), with: nil), true)
+    XCTAssertEqual(view.point(inside: CGPoint(x: -5, y: 50), with: nil), true)
+    XCTAssertEqual(view.point(inside: CGPoint(x: 105, y: 50), with: nil), true)
+    XCTAssertEqual(view.point(inside: CGPoint(x: 50, y: 105), with: nil), true)
+    
+    XCTAssertEqual(view.accessibilityFrame.contains(CGPoint(x: 50, y: -5)), false)
+    XCTAssertEqual(view.accessibilityFrame.contains(CGPoint(x: -5, y: 50)), false)
+    XCTAssertEqual(view.accessibilityFrame.contains(CGPoint(x: 105, y: 50)), false)
+    XCTAssertEqual(view.accessibilityFrame.contains(CGPoint(x: 50, y: 105)), false)
+  }
 
 }
